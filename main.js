01a3d25be86c6b0e29d5031a9aaed5801dd88ed8
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-07-30 12:22:42 +0200

    done main exercise

--- main.js
+++ main.js
@@ -1,27 +1,45 @@
-Vue.createapp({
+"use strict";
+
+const {createApp} = Vue;
+
+const app = createApp({
 
     data() {
 
         return {
 
-            url : "https://flynn.boolean.careers/exercises/api/random/mail",
+            emailAddresses: [],
 
         }
     },
 
     methods: {
 
+        getDataFromApi () {
+
+            for(let i = 0; i < 10; i++) {
+
+                axios.get(
+                    "https://flynn.boolean.careers/exercises/api/random/mail"
+                ).then( (axiosResponse) => {
+    
+                this.emailAddresses.push(axiosResponse.data.response);
+    
+                console.log(this.emailAddresses);
+    
+                });
+
+            }
+
+        }
+
     },
 
-    beforemount() {
+    mounted() {
         
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then( function (response) {
+        this.getDataFromApi();
 
-            console.log(response);
-
-        } )
-
-    },
+        }
 
 
 }).mount("#app");
